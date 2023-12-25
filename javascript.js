523a64b5b4cef0000675e0e7719b6aa3f4be1fec
tarun <venkatasaitarunmunduri@gmail.com>
2023-12-25 01:29:25 -0500

    Add funciton for the rock,paper,scissor game logic

--- javascript.js
+++ javascript.js
@@ -12,3 +12,21 @@ function getComputerChoice(){
     }
 }
 
+function play(playerSelection, computerSelection){
+    if(playerSelection==computerSelection){
+        return(["Tie!",0])
+    }
+    if((playerSelection=="Paper" && computerSelection=="Rock")){
+        return(["You Won! "+playerSelection +" beats "+ computerSelection,1]);
+    }
+    else if((playerSelection=="Scissors" && computerSelection=="Paper")){
+        return(["You Won! "+playerSelection +" beats "+ computerSelection,1]);
+    }
+    else if((playerSelection=="Rock" && computerSelection=="Scissors")){
+        return(["You Won! "+playerSelection +" beats "+ computerSelection,1]);
+    }
+    else{
+        return(["You Lose! "+computerSelection +" beats "+ playerSelection,-1]);
+    }
+
+}
