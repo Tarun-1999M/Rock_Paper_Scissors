2b309600b71ef4611162721cbb707a627c33c243
tarun <venkatasaitarunmunduri@gmail.com>
2023-12-25 01:28:38 -0500

    Add funciton to get computer choice

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    switch(Math.floor(Math.random()*3)){
+    case 0:
+        return("Rock");
+        break;
+    case 1:
+        return("Paper");
+        break;
+    case 2:
+        return("Scissors");
+        break;
+    }
+}
+
